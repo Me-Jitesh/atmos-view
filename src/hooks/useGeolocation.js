d47dcd2a6b84b15c104dcdc78967666a57a15a83
Jitesh Singh <jiteshs101@gmail.com>
2026-03-24 20:45:30 +0530

    Fetching GPS Coordinates

---
+++ src/hooks/useGeolocation.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+export default function useGeolocation() {
+  const [location, setLocation] = useState({
+    latitude: null,
+    longitude: null,
+  });
+
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!navigator.geolocation) {
+      setError("Geolocation not supported");
+      setLoading(false);
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setLocation({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+        setLoading(false);
+      },
+      (err) => {
+        setError(err.message);
+        setLoading(false);
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 5000,
+      },
+    );
+  }, []);
+
+  return { location, error, loading };
+}
--- src/pages/CurrentWeather.jsx
+++ src/pages/CurrentWeather.jsx
@@ -1,5 +1,18 @@
+import useGeolocation from "../hooks/useGeolocation";
+
 function CurrentWeather() {
-  return <div>Current Weather Page</div>;
+  const { location, error, loading } = useGeolocation();
+
+  if (loading) return <p>Fetching location...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  return (
+    <div>
+      <h2>Current Weather</h2>
+      <p>Latitude: {location.latitude}</p>
+      <p>Longitude: {location.longitude}</p>
+    </div>
+  );
 }
 
 export default CurrentWeather;
